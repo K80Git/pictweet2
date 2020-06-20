7c8b7c58ed5222765c9e7aad5f765c6d6c1a4b79
K80Git <67084301+K80Git@users.noreply.github.com>
2020-06-20 09:55:21 +0900

    new & createhtml add

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,5 +10,17 @@ class TweetsController < ApplicationController
     #@tweet 変数に入ったものは、Tweetモデルクラスに追加されるnewメソッドを適応される。
   end
 
+  def create
+    Tweet.create(tweet_params)
+    #tweetモデルを作る処理（tweet_paramsはプライベートに記載）
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+    #ストロングパラメータ　require（:tweet）とすることで、レスポンスで受け取る各データの内、tweetに関する内容を選ぶ。
+    #その後、tweetに入っている中の、name,image,textを受け取るとする。
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :tweets, only: [:index, :new]
+  resources :tweets, only: [:index, :new, :create]
   #resources にて自動でルートを設定できる→　indexとnewコントローラーに対し作成
 end
